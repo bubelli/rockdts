1a3baa6072ba0de50b596fc6a61c5fc38f6c75f7
bubelli <76003911+bubelli@users.noreply.github.com>
2021-01-02 19:57:11 +0100

    force and raotation applied

---
+++ Assets/Rocket.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    bool RotatingLeft = false;
+    bool RotatingRight = false;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        ProcessInput();
+    }
+    void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.AddRelativeForce(Vector3.up);
+            print("Thrust");
+        }
+        if (Input.GetKey(KeyCode.A) && RotatingRight == false)
+        {
+
+            transform.Rotate(Vector3.forward * 0.2f);
+
+        }
+        else if (Input.GetKey(KeyCode.D) && RotatingLeft == false)
+        {
+            transform.Rotate(-Vector3.forward * 0.2f);
+            RotatingRight = true;
+        }
+        RotatingLeft = false;
+        RotatingRight = false;
+    }
+}
